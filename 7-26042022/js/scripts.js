dff7f45f38655bf3a8454399c33b3de42356bbf9
Jefferson Cruz <jeffcruz1998@hotmail.com>
2022-04-26 17:08:57 -0300

    atualização

---
+++ 7-26042022/js/scripts.js
@@ -0,0 +1,26 @@
+function CarregarProdutos() {
+  // fazer o carregamento da url com filmes
+  // vamos usar o comando fetch para localizar e
+  // carregar os filmes e exibir em pagina html
+  fetch(
+    "https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
+  )
+    .then((response) => response.json())
+    .then((dados) => {
+      console.log(dados);
+      var saida = "<div id=produto>";
+      dados.map((item, ix) => {
+        saida += `
+        <div>
+        <img src=${item.image_link}>
+        <a href="${item.product_link}">${item.name}</a>
+        <p class=avaliacao>${item.rating}</p>
+        <p class=preco>R$ ${item.price}</p>
+        </div>
+      `;
+      });
+      saida += "</div>";
+      document.body.innerHTML += saida;
+    })
+    .catch((erro) => console.error(`erro ao tentar executar a api -> ${erro}`));
+}
